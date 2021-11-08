6fc426c9eea0c3f54c7475d65a56c69e6a17d014
William Minidis <wilmi895@student.liu.se>
2021-11-08 18:13:10 +0100

    Refactored code, moved functionality to relevant files.

--- initgame.py
+++ initgame.py
@@ -1,21 +1,32 @@
-"""
-"""
-import pygame
-import pymunk
+import pygame as pyg
+from initengine import *
 
-#----- Initialisation -----#
+import maps
+import gameobjects as gameobj
+import images
+import ai
+import initfunctions as init
 
-# -- Initialise the display
-pygame.init()
-pygame.display.set_mode()
+# -- Variables
+#   Define the current level
+current_map = maps.map0
+#   List of all game objects
+game_objects = []
+tanks = []
 
-# -- Initialise the clock
-clock = pygame.time.Clock()
+# -- Resize the screen to the size of the current level
+screen = pyg.display.set_mode(current_map.rect().size)
 
-# -- Initialise the physics engine
-space = pymunk.Space()
-space.gravity = (0.0,  0.0)
-space.damping = 0.1  # Adds friction to the ground for all objects
+# -- Generate the background
+background = init.create_grass_background(current_map, screen)
 
-#-- Constants
-FRAMERATE = 50
+game_objects = init.create_boxes(current_map, space)
+tanks = init.create_tanks(current_map, space)
+flag = init.create_flag(current_map)
+bases = init.create_bases(current_map)
+space.add(*init.create_map_bounds(current_map, space.static_body))
+
+
+space.add_collision_handler(1, 2).pre_solve = init.collision_bullet_tank
+space.add_collision_handler(1, 3).pre_solve = init.collision_bullet_box
+space.add_collision_handler(1, 0).pre_solve = init.collision_bullet_wall

1e1a6895aa028adbc9655783d7da6600d658099a
William Minidis <wilmi895@student.liu.se>
2021-12-06 17:52:23 +0100

    Added ascii art when starting game!

--- ctf.py
+++ ctf.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 """
 """
-import maps
 import pygame as pyg
+from maps import CTFMap
 from sounds import CTFSounds
 from images import CTFImages
 from ctfgame import CTFGame
 from cmdparser import parse_game_cmd_args
+from config import TERMINAL_DISPLAY_TITLE
 
+print(TERMINAL_DISPLAY_TITLE)
+arguments = parse_game_cmd_args()
 
 # Init the display
 pyg.init()
@@ -18,7 +21,5 @@ CTFImages()
 CTFSounds()
 CTFSounds.background.play()
 
-arguments = parse_game_cmd_args()
-
-game = CTFGame(arguments.game_mode, maps.CTFMap.load_map(arguments.map))
+game = CTFGame(arguments.game_mode, CTFMap.load_map(arguments.map))
 game.run_loop()
